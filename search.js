5308ab3b27ad407e014b22663d7040fb2da45977
Peeter Mäeots <maeots.peeter@gmail.com>
2018-05-29 18:08:56 +0300

    added js file and jQuery

---
+++ search.js
@@ -0,0 +1,19 @@
+// Youtube essentials
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyD7fadOdlYLbtAoWr8EqYzPpKMJS185u_c';
+
+// Get search terms from user
+
+// Authorise the request to Youtube
+
+// Get data from Youtube
+
+// Render the results to user
+
+// Search application jQuery callback
+function searchSubmitAction () {
+	console.log("I am ready to search");
+}
+
+// Initialize the search application on page load
+$(searchSubmitAction);
