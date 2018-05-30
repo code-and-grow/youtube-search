dc815a891853edfe12f9d972b7f6d8d02d7ef645
Peeter Mäeots <maeots.peeter@gmail.com>
2018-05-30 14:59:04 +0300

    added More from link to results

--- search.js
+++ search.js
@@ -18,12 +18,14 @@ function getDataFromYtube(searchTerm, callback) {
 }
 
 function renderResult(result) {
+  console.log(result);
   return `
     <a href="https://youtu.be/${result.id.videoId}" target="_blank">
       <h3 class="no-underline">${result.snippet.title}</h3>
       <img src="${result.snippet.thumbnails.high.url}" alt="${result.snippet.description}">
       <p>${result.snippet.description}</p>
     </a>
+    <p class="channel-link"><a href="https://www.youtube.com/channel/${result.snippet.channelId}">>>> More from ${result.snippet.channelTitle}</a></p>
   `;
 }
 
